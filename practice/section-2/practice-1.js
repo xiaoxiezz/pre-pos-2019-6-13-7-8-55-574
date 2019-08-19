835426369e6a816468bd4061489c6a13452fa80c
xiexinfang <1036554353@qq.com>
2019-08-19 20:16:31 +0800

    parctice-2

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,5 +1,19 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var res = [];
+  var map = new Map();
+   for (var i=0;i<collection.length;i++){
+     if (map.get(collection[i])==null){
+       map.set(collection[i],1);
+     }else{
+       map.set(collection[i],map.get(collection[i])+1);
+     }
+   }
+   //console.log(map);
+   map.forEach(function(key,value){
+     res.push({key:value,count:key})
+   });
+   console.log(res);
+   return res;
 }
