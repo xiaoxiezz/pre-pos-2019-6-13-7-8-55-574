9439bb087b462e96d366112b6d262cd2df5d47b5
xiexinfang <1036554353@qq.com>
2019-08-21 10:03:14 +0800

    2

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -2,16 +2,14 @@
 
 function collectSameElements(collectionA, collectionB) {
   // return '实现练习要求，并改写该行代码。';
-  // var collectionA=[];
-  // var collectionB=[];
-  var result=[];
-  for(var i=0;i<collectionA.length;i++){
-    for(var j=0;j<collectionB.length;j++){
+  let result=[];
+  for(let i=0;i<collectionA.length;i++){
+    for(let j=0;j<collectionB.length;j++){
       if(collectionA[i]==collectionB[j]){
         result.push(collectionA[i]);
       }
     }
   }
-return result;
+  return result;
 }
 
